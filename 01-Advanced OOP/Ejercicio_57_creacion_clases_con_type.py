2f81766b962fa233980a2b66f0c96beb7c560f8d
Fernando Paniagua <fernando.paniagua.python@gmail.com>
2026-03-10 20:05:41 +0100

    Creación de clases con type

---
+++ 01-Advanced OOP/Ejercicio_57_creacion_clases_con_type.py
@@ -0,0 +1,18 @@
+# Es una definición de clase
+def xxx(self):
+    return self.nombre
+
+Cosa = type("Cosa", (object,), {"numero_maximo":10, "get_nombre":xxx})
+
+la_cosa = Cosa()
+# print(la_cosa.get_nombre()) # Error -> Aún no tenemos atributo nombre
+la_cosa.nombre = 'Hulk'
+print(type(la_cosa))
+print(Cosa.numero_maximo)
+print(la_cosa.nombre)
+print(la_cosa.get_nombre())
+
+print('The class name is:', Cosa.__name__)
+print('The class is an instance of:', Cosa.__class__)
+print('The class is based on:', Cosa.__bases__)
+print('The class attributes are:', Cosa.__dict__)
--- 01-Advanced OOP/Ejercicios_49+_persistencia_oo/Ejercicio_56_modelo_shelve_http_volcado_y_consulta.py
+++ 01-Advanced OOP/Ejercicios_49+_persistencia_oo/Ejercicio_56_modelo_shelve_http_volcado_y_consulta.py
@@ -1,23 +1,40 @@
 import requests
+import shelve
+
+class Movie:
+    pass
 
 opcion = int(input('Introduce una opción (1-Crear fichero de persistencia; 2-Leer fichero de persistencia):'))
 
 if opcion==1:
+    peliculas = shelve.open('peliculas.sqlite', flag='c')
     reply = requests.get('http://localhost:8000/movies.json')
     status_code=reply.status_code
     if status_code==200:# La petición se ha atendido correctamente
         movie_list = reply.json() # movie_list es un array de diccionarios con datos de películas
         # print(type(movie_list)) # <class 'list'>
         for movie in movie_list:
-            print(movie['title'])
             # Crear un objeto con cada película
-
+            new_movie = Movie()
+            new_movie.__dict__ = movie
             # Guardar en un shelve (bbdd) cada una de las películas. La clave será el título.
-
+            peliculas[new_movie.title]=new_movie.__dict__
+            print(f'Película {new_movie.title} almacenada')
     else:
         print('Ha ocurrido un error:' + str(status_code))
+    peliculas.close()
 elif opcion==2:
+    peliculas = shelve.open('peliculas.sqlite', flag='r')
     while True:
-        titulo = input('Introduce un título:')
+        titulo = input('Introduce un título (Pulsa ENTER para salir):')
+        if titulo=='': break
         # Buscar la película en el fichero shelve y mostrar los datos por pantalla
+        if titulo in peliculas:
+            pelicula_encontrada = Movie()
+            pelicula_encontrada.__dict__=peliculas[titulo]
+            for k, v in pelicula_encontrada.__dict__.items():
+                print(f'{k}:{v}')
+        else:
+            print('No existe esa película, introduce otro título')
         # Si no existe, se advierte y se pide otra película.
+    peliculas.close()
