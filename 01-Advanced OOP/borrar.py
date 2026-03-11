47f15668d91fd614f6845d4c9b4d3985b8ae6617
Fernando Paniagua <fernando.paniagua.python@gmail.com>
2026-03-11 20:25:52 +0100

    Update borrar.py

--- 01-Advanced OOP/borrar.py
+++ 01-Advanced OOP/borrar.py
@@ -1,19 +1,9 @@
-import json
+def imprimir(*args):
+    for item in args:
+        print(item+' ', end='')
+    print()
 
-class Factura:
-    def __init__(self, cliente, importe):
-        self.cliente = cliente
-        self.importe = importe
 
-    def get_json_data(self):
-        return json.dumps({
-            "cliente": self.cliente,
-            "importe": self.importe
-        }, ensure_ascii=False)
-    
-factura = Factura('Rosalía',1000)
-
-json = factura.get_json_data()
-print(json)
+imprimir('hola','melón','sapo')
 
 
